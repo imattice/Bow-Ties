bf73bfcb27e3efbbe89b57b47b739d2a0b995f94
Ike <ike.mattice@tiger-sheep.com>
2016-12-20 16:54:44 -0800

    Initial Commit

---
+++ Bowtie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Bowtie+CoreDataClass.swift
+//  Bow Ties
+//
+//  Created by Ike Mattice on 12/20/16.
+//  Copyright © 2016 Razeware. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Bowtie: NSManagedObject {
+
+}
